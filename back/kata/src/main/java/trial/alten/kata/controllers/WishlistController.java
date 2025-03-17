649feb2c37a76d58520f47a18284fb7ffac096f2
mohammed.tourab <mohammed.tourab@digiup.ma>
2025-03-17 23:01:12 +0000

    add Swagger annotations.

--- back/kata/src/main/java/trial/alten/kata/controllers/WishlistController.java
+++ back/kata/src/main/java/trial/alten/kata/controllers/WishlistController.java
@@ -1,6 +1,7 @@
 package trial.alten.kata.controllers;
 
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +11,7 @@ import trial.alten.kata.services.WishlistService;
 
 @RestController
 @RequestMapping("/wishlist")
+@Tag(name = "Wishlist Operations", description = "Endpoints for handling wishlist operations.")
 public class WishlistController {
 
     private final WishlistService wishlistService;
